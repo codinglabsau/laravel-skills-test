d7ff3251bb885afe923011d0aeafadf5c5e3f89e
vrodriguero <vickyamauti@hotmail.com>
2020-08-10 18:13:30 +1000

    add post table migration, database seeder

---
+++ database/migrations/2020_08_10_051630_create_posts_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes()->nullable();
+        });
+    }
+}
