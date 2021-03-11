8dd4d5a457120fd8d49e81c9fedfef464937b8c4
Jesse Tao <c.tao@griffith.edu.au>
2021-03-11 21:33:51 +1000

    add image resize in aliases

--- config/app.php
+++ config/app.php
@@ -213,6 +213,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Image' => Intervention\Image\Facades\Image::class,
+
     ],
 
 ];
