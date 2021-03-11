21bc793d683696cdb59b96ae27d458b36654b97f
Jesse Tao <c.tao@griffith.edu.au>
2021-03-11 21:32:35 +1000

    add ImageServiceProvider

--- config/app.php
+++ config/app.php
@@ -160,6 +160,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // ImageServiceProvider
+        Intervention\Image\ImageServiceProvider::class
+
+
     ],
 
     /*
